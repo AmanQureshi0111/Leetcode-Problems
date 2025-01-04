99e8aa6bc5c16b53f6e609bb79ea4ed47eaf29d9
AmanQureshi0111 <amanq7362@gmail.com>
2025-01-04 15:17:51 +0530

    update POTD string

--- String/1930. Unique Length-3 Palindromic Subsequences.cpp
+++ String/1930. Unique Length-3 Palindromic Subsequences.cpp
@@ -32,3 +32,39 @@ public:
         return result;
     }
 };
+
+/*  method-2 
+    TC-O(n) SC-O(26)
+*/
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        vector<pair<int,int>> indices(26,{-1,-1});
+        for(int i=0;i<s.length();i++){
+            int idx=s[i]-'a';
+            if(indices[idx].first==-1){
+                indices[idx].first=i;
+            }
+            indices[idx].second=i;
+        }
+        int result=0;
+        for(int i=0;i<26;i++){
+            int first_idx=indices[i].first;
+            int last_idx=indices[i].second;
+            /*for(int i=0;i<s.length();i++){
+                if(s[i]==ch){
+                    if(first_idx==-1){
+                        first_idx=i;
+                    }
+                    last_idx=i;
+                }
+            }*/
+            unordered_set<char> st;
+            for(int middle=first_idx+1;middle<last_idx;middle++){
+                st.insert(s[middle]);
+            }
+            result+=st.size();
+        }
+        return result;
+    }
+};
